4b2b3655597a98182fd250a9e01f52df7ec701bf
Leandro Sarracini <lsarraci@c1r3p3.42sp.org.br>
2026-01-23 19:15:42 -0300

    modify: alterações nas funções de exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,75 +6,78 @@
 /*   By: lsarraci <lsarraci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 13:51:29 by lsarraci          #+#    #+#             */
-/*   Updated: 2026/01/23 17:58:27 by lsarraci         ###   ########.fr       */
+/*   Updated: 2026/01/23 19:08:08 by lsarraci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/shell.h"
 
-static void	print_numeric(char *cmd, t_env *env)
-{
-	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-	ft_putstr_fd(cmd, STDERR_FILENO);
-	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-	env->should_exit = 1;
-	env->exit_code = 255;
-}
-
-static int	return_exit_code(char **str, t_env *env)
-{
-	int	code;
-
-	if (!str || !str[1])
-		code = 0;
-	else if (is_numeric(str[1]))
-		code = ft_atoi(str[1]);
-	else
-		print_numeric(str[1], env);
-	ft_free_split(str);
-	return (code % 256);
-}
-
-static int	check_valid_args(int argc, char **args)
+static int	is_numeric_arg(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (args[1][i])
+	if (!str)
+		return (0);
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
 	{
-		if (!is_numeric(args[1]))
-		{
-			print_numeric(args[1], NULL);
+		if (!ft_isdigit(str[i]))
 			return (0);
-		}
 		i++;
 	}
-	if (argc > 2)
-	{
-		ft_putstr_fd("minishell: too many arguments\n", STDERR_FILENO);
-		return (0);
-	}
 	return (1);
+}
+
+static int	return_exit_code(char **str)
+{
+	int	code;
+
+	code = 0;
+	if (!str || !str[1])
+		code = 0;
+	else if (is_numeric_arg(str[1]))
+		code = ft_atoi(str[1]);
+	else
+		code = 255;
+	return (code % 256);
+}
+
+static void	return_numeric_error(char *arg)
+{
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
 }
 
 int	builtin_exit(char **args, t_env *env)
 {
 	int	arg_count;
-	int	valid;
+	int	exit_code;
 
-	valid = 1;
 	arg_count = count_args(args);
+	exit_code = 0;
 	ft_printf("exit\n");
-	if (arg_count == 1)
+	if (arg_count > 2)
 	{
-		env->should_exit = 1;
-		env->exit_code = return_exit_code(args, env);
-		return (0);
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		env->should_exit = 0;
+		env->exit_code = 1;
+		return (1);
 	}
-	valid = check_valid_args(count_args(args), args);
-	if (!valid)
-		return (0);
+	if (arg_count > 1 && !is_numeric_arg(args[1]))
+	{
+		return_numeric_error(args[1]);
+		env->should_exit = 1;
+		env->exit_code = 255;
+		return (255);
+	}
+	exit_code = return_exit_code(args);
 	env->should_exit = 1;
-	env->exit_code = return_exit_code(args, env);
-	return (0);
+	env->exit_code = exit_code;
+	ft_printf("terminated with exit code: %d\n", exit_code);
+	return (exit_code);
 }
